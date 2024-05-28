6aa4184c261ddef15448e7299a8e570508e3c965
Ahmed Nasr <work@ahmednasr.at>
2024-05-28 09:42:17 +0200

    add getTrendingTweets service with task description

---
+++ backend/src/services/getTrendingTweets.js
@@ -0,0 +1,12 @@
+/*
+
+Discover Page mit Trending Posts
+    
+Schreibe eine Funktion getTrendingTweets() welche die Top 30 Tweets der letzen Woche, nach dem Zwitscher-Score sortiert, liefert.
+Der Zwitscher-Score besteht aus Anzahl der Comments zu einem Tweet geteilt durch die Lebensdauer des Tweets.
+Hinweis: Wenn ein Tweet älter als 1ne Woche ist, dann wird er ignoriert (egal wie populär, zählt nicht mehr als Trending).
+
+
+*/
+
+export async function getTrendingTweets() {}
