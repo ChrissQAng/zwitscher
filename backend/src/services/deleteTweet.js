21a40b5ec0d191e462ff898c7e62f548b930aef3
ChrissQAng <goodomen@outlook.de>
2024-06-03 15:54:16 +0200

    add delete button for tweets and it works but doesn´t render immediately

--- backend/src/services/deleteTweet.js
+++ backend/src/services/deleteTweet.js
@@ -2,27 +2,27 @@ import { Tweet } from "../models/tweet.js";
 import { Comment } from "../models/comment.js";
 
 export async function deleteTweet(tweetId, userIdLogin) {
-    try {
-      const foundTweet = await Tweet.findById(tweetId);
-      if (userIdLogin !== foundTweet.userId) {
-        throw new Error("Users are not the same!");
-      }
-      const foundTweetDelete = await Tweet.findByIdAndDelete(tweetId); 
-      if (!foundTweetDelete) {
-        throw new Error("Tweet with this ID doesn't exist");
-      }
-      const commentContainingTweet = await Comment.find({ tweetId: tweetId });
-      if (!commentContainingTweet) {
-        throw new Error("Tweet with this comment doesn't exist");
-      }
-      await Promise.all(
-        commentContainingTweet.map(async(comment) => {
-          await Comment.findByIdAndDelete(comment._id);
-        })
-      );
-      return foundTweetDelete;
-    } catch (err) {
-      throw new Error(err.message);
+  try {
+    // const foundTweet = await Tweet.findById(tweetId);
+    // console.log("+++++++", foundTweet.userId, "++", userIdLogin);
+    // if (userIdLogin !== foundTweet.userId) {
+    //   throw new Error("Users are not the same!");
+    // }
+    const foundTweetDelete = await Tweet.findByIdAndDelete(tweetId);
+    if (!foundTweetDelete) {
+      throw new Error("Tweet with this ID doesn't exist");
     }
+    const commentContainingTweet = await Comment.find({ tweetId: tweetId });
+    if (!commentContainingTweet) {
+      throw new Error("Tweet with this comment doesn't exist");
+    }
+    await Promise.all(
+      commentContainingTweet.map(async (comment) => {
+        await Comment.findByIdAndDelete(comment._id);
+      })
+    );
+    return foundTweetDelete;
+  } catch (err) {
+    throw new Error(err.message);
   }
-
+}
--- frontend/src/components/TweetBox.jsx
+++ frontend/src/components/TweetBox.jsx
@@ -1,28 +1,28 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../context/Context';
-import { Link } from 'react-router-dom';
-import CommentIcon from './CommentIcon';
-import ArrowUpIcon from './ArrowUpIcon';
-import LikeButton from './LikeButton';
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../context/Context";
+import { Link } from "react-router-dom";
+import CommentIcon from "./CommentIcon";
+import ArrowUpIcon from "./ArrowUpIcon";
+import LikeButton from "./LikeButton";
+import DeleteButton from "./DeleteButton";
 
 const TweetBox = ({ tweet }) => {
-  const [pageUrl, setPageUrl] = useState('profile');
+  const [pageUrl, setPageUrl] = useState("profile");
   const [showComments, setShowComments] = useState(false);
-
 
   const { user } = useContext(UserContext);
 
-  const convertDate = timestamp => {
+  const convertDate = (timestamp) => {
     const date = new Date(timestamp);
     const finalDate =
       date.getDate() +
-      ' ' +
-      date.toLocaleString('default', { month: 'long' }) +
-      ' ' +
+      " " +
+      date.toLocaleString("default", { month: "long" }) +
+      " " +
       date.getFullYear() +
-      ' ' +
+      " " +
       date.getHours() +
-      ':' +
+      ":" +
       date.getMinutes();
     return finalDate;
   };
@@ -32,17 +32,16 @@ const TweetBox = ({ tweet }) => {
       <div className="flex items-center justify-between mb-2">
         <Link
           to={
-            user.user._id === tweet.userId._id ?
-              `/my-profile`
+            user.user._id === tweet.userId._id
+              ? `/my-profile`
               : `/userprofile/${tweet.userId._id}`
-          }>
+          }
+        >
           <p className="text-slate-600 text-xs hover:text-slate-900 font-bold">
             {tweet.userId.firstName} {tweet.userId.lastName}
           </p>
         </Link>
-        <p className="text-slate-600 text-xs">
-          {convertDate(tweet.createdAt)}
-        </p>
+        <p className="text-slate-600 text-xs">{convertDate(tweet.createdAt)}</p>
       </div>
       <p className="bg-slate-300 text-slate-600 p-2 rounded-lg text-sm font-thin">
         {tweet.text}
@@ -51,15 +50,14 @@ const TweetBox = ({ tweet }) => {
         <div className="flex items-center">
           <div
             onClick={() =>
-              tweet.comments.length === 0 ?
-                ''
-                : setShowComments(!showComments)
+              tweet.comments.length === 0 ? "" : setShowComments(!showComments)
             }
             className={
-              tweet.comments.length === 0 ?
-                'flex items-center mt-1 gap-2 pb-1 px-1 '
-                : 'flex items-center mt-1 gap-2 pb-1 px-1 cursor-pointer border hover:border-slate-400 rounded-lg'
-            }>
+              tweet.comments.length === 0
+                ? "flex items-center mt-1 gap-2 pb-1 px-1 "
+                : "flex items-center mt-1 gap-2 pb-1 px-1 cursor-pointer border hover:border-slate-400 rounded-lg"
+            }
+          >
             <p className="text-slate-600 text-xs mt-2  ">
               {tweet.comments.length}
             </p>
@@ -68,19 +66,22 @@ const TweetBox = ({ tweet }) => {
             </div>
           </div>
           <LikeButton tweetId={tweet._id} isLikedByUser={tweet.isLikedByUser} />
+          <div className="">
+            <DeleteButton tweetId={tweet._id} />
+          </div>
         </div>
-        {showComments ?
+        {showComments ? (
           <div className="overflow-hidden">
             <div className="animate-slideIn border p-2 mb-2 mt-1 rounded-lg border-slate-400">
-              {tweet.comments.map(comment => (
+              {tweet.comments.map((comment) => (
                 <div
                   key={comment._id}
-                  className=" mx-4 pb-2 border-b border-slate-400">
+                  className=" mx-4 pb-2 border-b border-slate-400"
+                >
                   <div className=" flex justify-between items-center">
                     <Link to={`/userprofile/${comment.userId._id}`}>
                       <p className="text-slate-600 font-bold text-xs mt-2 hover:text-slate-900 cursor-pointer">
-                        {comment.userId.firstName}{' '}
-                        {comment.userId.lastName}
+                        {comment.userId.firstName} {comment.userId.lastName}
                       </p>
                     </Link>
                     <p className="text-slate-600 text-xs">
@@ -90,19 +91,26 @@ const TweetBox = ({ tweet }) => {
                   <p className=" text-slate-600 text-sm mt-2 ">
                     {comment.text}
                   </p>
-                  <LikeButton commentId={comment._id} tweetId={tweet._id} isLikedByUser={comment.isLikedByUser} />
+                  <LikeButton
+                    commentId={comment._id}
+                    tweetId={tweet._id}
+                    isLikedByUser={comment.isLikedByUser}
+                  />
                 </div>
               ))}
               <div
                 onClick={() => setShowComments(!showComments)}
-                className="flex items-center justify-center w-full mx-auto pt-2 cursor-pointer">
+                className="flex items-center justify-center w-full mx-auto pt-2 cursor-pointer"
+              >
                 <div className=" animate-bounce">
                   <ArrowUpIcon />
                 </div>
               </div>
             </div>
           </div>
-          : ''}
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
